c2cadf015f08e45dadf1ee507b62b8b80fd42044
Michael-Lee-1994 <michael.lee.1994.flatiron@gmail.com>
2020-11-16 15:18:34 -0800

    commit tree 587c854c0b3812893c42b32eae17f029a6f54149

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(a,b) {
+    let temp = []
+    for(let i = 0; i < a.length; i++) {
+        temp.push(`Thank you, ${a[i]}, for the wonderful ${b} gift!`)
+    }
+    return temp;
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
